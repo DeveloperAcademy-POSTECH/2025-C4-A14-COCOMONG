63b508ea721c3c6cd1918d03fb5741506bb9dc22
n_joid <72697791+nakisara01@users.noreply.github.com>
2025-07-17 21:39:53 +0900

    [#4] design: ChooseCycleView, ChooseCycleInfoView 구현

--- Beat100/WatchBeat100 Watch App/Resources/Extension/Font+Extensions.swift
+++ Beat100/WatchBeat100 Watch App/Resources/Extension/Font+Extensions.swift
@@ -32,37 +32,7 @@ extension Font {
         }
     }
     
-    enum SFCompact {
-        case light
-        case regular
-        case medium
-        case semibold
-        case bold
-        case heavy
-        
-        var value: String {
-            switch self {
-            case .light:
-                return "SFCompactText-Light"
-            case .regular:
-                return "SFCompactText-Regular"
-            case .medium:
-                return "SFCompactText-Medium"
-            case .semibold:
-                return "SFCompactText-Semibold"
-            case .bold:
-                return "SFCompactText-Bold"
-            case .heavy:
-                return "SFCompactText-Heavy"
-            }
-        }
-    }
-    
     static func nanumSquareNeo(type: NanumSquareNeo, size: CGFloat) -> Font {
-        return .custom(type.value, size: size)
-    }
-    
-    static func sfCompact(type: SFCompact, size: CGFloat) -> Font {
         return .custom(type.value, size: size)
     }
 }
