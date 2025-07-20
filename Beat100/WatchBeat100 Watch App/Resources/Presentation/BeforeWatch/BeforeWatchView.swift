cbacdd26a83f6c7b0a1c2f3d9fa68288fe7f61b2
n_joid <72697791+nakisara01@users.noreply.github.com>
2025-07-20 18:27:46 +0900

    [#13] Design: BeforeWatchView 구현 및 디자인 에셋 추가

---
+++ Beat100/WatchBeat100 Watch App/Resources/Presentation/BeforeWatch/BeforeWatchView.swift
@@ -0,0 +1,33 @@
+//
+//  BeforeWatchView.swift
+//  WatchBeat100 Watch App
+//
+//  Created by 나현흠 on 7/20/25.
+//
+
+import SwiftUI
+
+struct BeforeWatchView: View {
+    var body: some View {
+        VStack{
+            ZStack{
+                Image("iPhone_System")
+                    .padding(.trailing, 20)
+                Image("Beat100_WhiteLogo")
+                    .padding(.leading, 35)
+                    .padding(.top, 25)
+            }
+            .padding(.bottom, 16)
+            Text("CPR 측정을 설정하려면\niPhone에서 Beat100앱을\n사용해주세요.")
+                .font(.system(size: 11, weight: .regular))
+                .multilineTextAlignment(.center)
+        }
+        .toolbar{}
+    }
+    
+}
+
+#Preview {
+    BeforeWatchView()
+}
+
