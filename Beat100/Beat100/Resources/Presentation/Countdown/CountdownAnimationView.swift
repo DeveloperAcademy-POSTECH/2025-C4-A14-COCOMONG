1535e1e35bf4e2c25961eb15b218ce713432d0f7
ohesko <ohesko@gmail.com>
2025-07-22 20:48:07 +0900

    [#19] Chore: CountdownAnimationView 파일 생성

---
+++ Beat100/Beat100/Resources/Presentation/Countdown/CountdownAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  CountdownAnimationView.swift
+//  Beat100
+//
+//  Created by Seungah Ko on 7/22/25.
+//
+
+import SwiftUI
+
+struct CountdownAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountdownAnimationView()
+}
