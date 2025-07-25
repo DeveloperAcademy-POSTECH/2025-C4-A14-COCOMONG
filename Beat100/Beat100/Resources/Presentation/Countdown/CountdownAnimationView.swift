0e8fa9e42d58106332035dcb3f3902319fb3c419
n_joid <72697791+nakisara01@users.noreply.github.com>
2025-07-25 15:38:42 +0900

    [#38] Refactor: git flow 오류 해결

--- Beat100/Beat100/Resources/Presentation/Countdown/CountdownAnimationView.swift
+++ Beat100/Beat100/Resources/Presentation/Countdown/CountdownAnimationView.swift
@@ -8,100 +8,80 @@
 import SwiftUI
 
 struct CountdownAnimationView: View {
-    @State private var count: Int = 4
-    @State private var timer: Timer?
-    @State private var showCircle: Bool = false
-    @State private var fillAmount: CGFloat = 0.0
+    @StateObject var viewModel:CountdownViewModel
+    var onComplete: () -> Void
     
     var body: some View {
-        
-        ZStack {
-            Color.black.ignoresSafeArea()
-            
-            VStack{
-                ZStack{
-                    //Background Circle
-                    CustomGradientCircle(progress: 100, width: 20)
-                        .opacity(showCircle ? 0.2 : 0.0)
-                        .frame(width: 220, height: 220)
-                        .animation(.easeInOut(duration: 2.0), value: showCircle)
-                        .opacity(showCircle ? 1.0 : 0.0)
-                    
-                    //Foreground Circle
-                    CustomGradientCircle(progress: fillAmount, width: 20)
-                        .frame(width: 220, height: 220)
-                        .opacity(showCircle ? 1.0 : 0.0)
-                        .animation(.easeInOut(duration: 1.0), value: fillAmount)
-                        .animation(.easeInOut(duration: 1.0), value: showCircle)
-                        .rotationEffect(.degrees(count == 4 ? 0 : 0))
-                    
-                    if count == 4 {
-                        Text("준비")
-                            .font(.nanumSquareNeo(type: .heavy, size: 50))
-                            .foregroundStyle(.white)
-                    } else {
-                        Text(countText)
+        NavigationStack{
+            ZStack {
+                Color.black.ignoresSafeArea(edges: .all)
+                
+#if os(iOS)
+                VStack{
+                    ZStack{
+                        //Background Circle
+                        CustomGradientCircle(progress: 100, width: 20)
+                            .opacity(viewModel.showCircle ? 0.2 : 0.0)
+                            .frame(width: 220, height: 220)
+                            .animation(.easeInOut(duration: 2.0), value: viewModel.showCircle)
+                            .opacity(viewModel.showCircle ? 1.0 : 0.0)
+                        
+                        //Foreground Circle
+                        CustomGradientCircle(progress: viewModel.fillAmount, width: 20)
+                            .frame(width: 220, height: 220)
+                        
+                        Text(viewModel.countText)
                             .font(.nanumSquareNeo(type: .heavy, size: 90))
                             .foregroundStyle(.white)
                     }
-                }
                     .padding(.bottom, 28)
                     Text("CPR 측정")
                         .foregroundStyle(.white)
                         .font(.system(size: 20, weight: .bold))
                 }
-            }
-            .onAppear {
-                startCountdown()
-            }
-        }
-    
-    var countText: String {
-        switch count {
-        case 4: return "준비"
-        case 3, 2, 1: return "\(count)"
-        default: return ""
-        }
-    }
-    
-    func startCountdown() {
-        count = 4
-        fillAmount = 0.0
-        showCircle = false
-
-        // 준비 시: 0 → 1.0 채우기
-        withAnimation(.easeInOut(duration: 1.0)) {
-            fillAmount = 1.0
-            showCircle = true
-        }
-
-        var step = 0
-
-        // 1초 뒤부터 카운트다운
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { t in
-                if step <= 3 {
-                    count = 3 - step
-                    step += 1
-
-                    // 진행 링 줄이기
-                    withAnimation(.easeInOut(duration: 0.8)) {
-                        switch count {
-                        case 3: fillAmount = 0.66
-                        case 2: fillAmount = 0.33
-                        case 1: fillAmount = 0.0
-                        default: break
-                        }
+#elseif os(watchOS)
+                VStack{
+                    ZStack{
+                        //Background Circle
+                        CustomGradientCircle(progress: 100, width: 12)
+                            .opacity(viewModel.showCircle ? 0.2 : 0.0)
+                            .frame(width: 126, height: 126)
+                            .opacity(viewModel.showCircle ? 1.0 : 0.0)
+                        
+                        //Foreground Circle
+                        CustomGradientCircle(progress: viewModel.fillAmount, width: 12)
+                            .frame(width: 126, height: 126)
+                        
+                        Text(viewModel.countText)
+                            .font(.nanumSquareNeo(type: .heavy, size: 32))
+                            .foregroundStyle(.white)
                     }
-                } else {
-                    t.invalidate()
-                    // 측정 중
+                    .padding(.bottom, 14)
+                    Text("CPR 측정")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 12, weight: .bold))
+                }
+#endif
+            }
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("") {
+                    }
+                    .tint(Color.black)
+                    .disabled(true)
                 }
             }
         }
+        .onAppear {
+            viewModel.onComplete = {
+                onComplete()
+            }
+            viewModel.startCountdown()
+        }
     }
+    
 }
 
 #Preview {
-    CountdownAnimationView()
+    CountdownAnimationView(viewModel: CountdownViewModel(), onComplete: {})
 }
--- Beat100/Beat100/Resources/Presentation/Countdown/CountdownViewModel.swift
+++ Beat100/Beat100/Resources/Presentation/Countdown/CountdownViewModel.swift
@@ -6,28 +6,59 @@
 //
 
 import Foundation
+import SwiftUI
 import Combine
 
 class CountdownViewModel: ObservableObject {
-    @Published var countdown: Int = 3
-    @Published var isCountdownDone: Bool = false
-    private var cancellables: Set<AnyCancellable> = []
+    @Published var showCircle: Bool = false
+    @Published var fillAmount: CGFloat = 0.0
+    @Published private var count: Int = 4
+    @State private var timer: Timer?
     
-    func startTimer(){
-        let timer = Timer.publish(every: 1, on: .main, in: .common)
-            .autoconnect()
-        timer
-            .sink { [weak self] _ in
-                guard let self = self else { return }
-                
-                if self.countdown > 0 {
-                    self.countdown -= 1
-                }
-                
-                if self.countdown == 0 {
-                    self.isCountdownDone = true
+    var onComplete: (() -> Void)?
+    
+    var countText: String {
+        switch count {
+        case 4: return "준비"
+        case 3, 2, 1: return "\(count)"
+        default: return ""
+        }
+    }
+    
+    func startCountdown() {
+        
+        // 준비 시: 0 → 1.0 채우기
+        withAnimation(.easeInOut(duration: 1.0)) {
+            fillAmount = 1.0
+            showCircle = true
+        }
+        
+        var step = 0
+        
+        // 1초 뒤부터 카운트다운
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { t in
+                if step <= 3 {
+                    self.count = 3 - step
+                    step += 1
+                    
+                    // 진행 링 줄이기
+                    withAnimation(.easeInOut(duration: 0.8)) {
+                        switch self.count {
+                        case 3: self.fillAmount = 0.66
+                        case 2: self.fillAmount = 0.33
+                        case 1: self.fillAmount = 0.0
+                        case 0:
+                            t.invalidate()
+                            self.onComplete?()   // **카운트 끝 → 콜백 호출**
+                        default: break
+                        }
+                    }
+                } else {
+                    t.invalidate()
+                    // 측정 중
                 }
             }
-            .store(in: &cancellables)
+        }
     }
 }
