135f25d132ce7a3dc192937bbd92df2b0ebaa1ce
n_joid <72697791+nakisara01@users.noreply.github.com>
2025-07-26 17:43:07 +0900

    [#36] Refactor: MeasuringCompleteView Component화

--- Beat100/Beat100/Resources/Presentation/Measuring/MeasuringCompleteView.swift
+++ Beat100/Beat100/Resources/Presentation/Measuring/MeasuringCompleteView.swift
@@ -8,60 +8,35 @@
 import SwiftUI
 
 struct MeasuringCompleteView: View {
+    var onComplete: () -> Void
+    private let config = MeasuringCompleteConfig.current
+    
     var body: some View {
-#if os(iOS)
-        ZStack{
+        ZStack {
             Color.black.ignoresSafeArea(edges: .all)
-            VStack{
+            VStack {
                 Text("측정완료")
                     .foregroundStyle(Color.white)
-                    .font(.system(size: 24, weight: .heavy))
-                HStack{
-                    Circle()
-                        .frame(width: 16, height: 16)
-                        .foregroundStyle(.beatPink)
-                        .padding(.trailing, 15)
-                    Circle()
-                        .frame(width: 16, height: 16)
-                        .foregroundStyle(.beatGradPink)
-                        .padding(.trailing, 15)
-                    Circle()
-                        .frame(width: 16, height: 16)
-                        .foregroundStyle(.beatBlue)
-                }
-                .frame(height: 130)
+                    .font(.system(size: config.fontSize, weight: .heavy))
+                
+                BouncingDotsView(dotSize: config.dotSize)
+                
                 Text("리포트를 생성중입니다")
                     .foregroundStyle(Color.white)
-                    .font(.system(size: 24, weight: .heavy))
+                    .font(.system(size: config.fontSize, weight: .heavy))
+            }
+            .padding(.top, config.topPadding)
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("") { onComplete() }
+                        .tint(Color.black)
+                        .disabled(true)
+                }
             }
         }
-#elseif os(watchOS)
-        VStack{
-            Text("측정완료")
-                .foregroundStyle(Color.white)
-                .font(.system(size: 12, weight: .heavy))
-            HStack{
-                Circle()
-                    .frame(width: 10, height: 10)
-                    .foregroundStyle(.beatPink)
-                    .padding(.trailing, 10)
-                Circle()
-                    .frame(width: 10, height: 10)
-                    .foregroundStyle(.beatGradPink)
-                    .padding(.trailing, 10)
-                Circle()
-                    .frame(width: 10, height: 10)
-                    .foregroundStyle(.beatBlue)
-            }
-            .frame(height: 78)
-            Text("리포트를 생성중입니다")
-                .foregroundStyle(Color.white)
-                .font(.system(size: 12, weight: .heavy))
-        }
-#endif
     }
 }
 
 #Preview {
-    MeasuringCompleteView()
+    MeasuringCompleteView(onComplete: {})
 }
--- Beat100/Beat100/Resources/Util/Config.swift
+++ Beat100/Beat100/Resources/Util/Config.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct MeasuringCompleteConfig {
+    let dotSize: CGFloat
+    let fontSize: CGFloat
+    let topPadding: CGFloat
+    
+    static var current: MeasuringCompleteConfig {
+        #if os(iOS)
+        return MeasuringCompleteConfig(dotSize: 16, fontSize: 24, topPadding: 0)
+        #elseif os(watchOS)
+        return MeasuringCompleteConfig(dotSize: 10, fontSize: 12, topPadding: 20)
+        #endif
+    }
+}
