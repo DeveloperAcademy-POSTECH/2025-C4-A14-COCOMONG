5f413b6e1d3b38e92be35b7f0fdfffc3b4d6e734
n_joid <72697791+nakisara01@users.noreply.github.com>
2025-07-26 16:54:00 +0900

    [#38] Refactor: BouncingDots 컴포넌트화

---
+++ Beat100/Beat100/Resources/Common/Components/BouncingDotsView.swift
@@ -0,0 +1,34 @@
+//
+//  BouncingDotsView.swift
+//  Beat100
+//
+//  Created by 나현흠 on 7/26/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct BouncingDotsView: View {
+    var dotSize: CGFloat
+    
+    var body: some View {
+        TimelineView(.animation) { timeline in
+            let time = timeline.date.timeIntervalSinceReferenceDate
+            HStack {
+                ForEach(0..<3) { index in
+                    Circle()
+                        .frame(width: dotSize, height: dotSize)
+                        .foregroundStyle(index == 0 ? .beatPink : index == 1 ? .beatGradPink : .beatBlue)
+                        .offset(y: bounceOffset(for: index, time: time))
+                        .padding(.trailing, index < 2 ? dotSize < 12 ? 10 : 15 : 0)
+                }
+            }
+            .frame(height: dotSize < 12 ? 78 : 130)
+        }
+    }
+    
+    private func bounceOffset(for index: Int, time: TimeInterval) -> CGFloat {
+        let delay = Double(index) * 0.2
+        return sin((time + delay) * 4) * 5
+    }
+}
--- Beat100/Beat100/Resources/Presentation/Measuring/MeasuringCompleteView.swift
+++ Beat100/Beat100/Resources/Presentation/Measuring/MeasuringCompleteView.swift
@@ -18,19 +18,9 @@ struct MeasuringCompleteView: View {
                 Text("측정완료")
                     .foregroundStyle(Color.white)
                     .font(.system(size: 24, weight: .heavy))
-                TimelineView(.animation) { timeline in
-                    let time = timeline.date.timeIntervalSinceReferenceDate
-                    HStack {
-                        ForEach(0..<3) { index in
-                            Circle()
-                                .frame(width: 16, height: 16)
-                                .foregroundStyle(index == 0 ? .beatPink : index == 1 ? .beatGradPink : .beatBlue)
-                                .offset(y: bounceOffset(for: index, time: time))
-                                .padding(.trailing, index < 2 ? 15 : 0)
-                        }
-                    }
-                    .frame(height: 130)
-                }
+                
+                BouncingDotsView(dotSize: 16)
+                
                 Text("리포트를 생성중입니다")
                     .foregroundStyle(Color.white)
                     .font(.system(size: 24, weight: .heavy))
@@ -48,19 +38,9 @@ struct MeasuringCompleteView: View {
                 Text("측정완료")
                     .foregroundStyle(Color.white)
                     .font(.system(size: 12, weight: .heavy))
-                TimelineView(.animation) { timeline in
-                    let time = timeline.date.timeIntervalSinceReferenceDate
-                    HStack {
-                        ForEach(0..<3) { index in
-                            Circle()
-                                .frame(width: 10, height: 10)
-                                .foregroundStyle(index == 0 ? .beatPink : index == 1 ? .beatGradPink : .beatBlue)
-                                .offset(y: bounceOffset(for: index, time: time))
-                                .padding(.trailing, index < 2 ? 10 : 0)
-                        }
-                    }
-                    .frame(height: 78)
-                }
+                
+                BouncingDotsView(dotSize: 10)
+                
                 Text("리포트를 생성중입니다")
                     .foregroundStyle(Color.white)
                     .font(.system(size: 12, weight: .heavy))
@@ -78,11 +58,6 @@ struct MeasuringCompleteView: View {
             
 #endif
         }
-    }
-    
-    func bounceOffset(for index: Int, time: TimeInterval) -> CGFloat {
-        let delay = Double(index) * 0.2
-        return sin((time + delay) * 4) * 5
     }
 }
 
