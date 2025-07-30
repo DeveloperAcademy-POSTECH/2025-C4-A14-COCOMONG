569ed55fdbda769d074e3e41dcf2b5bad589c59e
n_joid <72697791+nakisara01@users.noreply.github.com>
2025-07-30 16:00:38 +0900

    [#67] Fix: MeasuringCompleteView QA 반영

--- Beat100/Beat100/Resources/Util/Config.swift
+++ Beat100/Beat100/Resources/Util/Config.swift
@@ -16,7 +16,7 @@ struct MeasuringCompleteConfig {
         #if os(iOS)
         return MeasuringCompleteConfig(dotSize: 16, fontSize: 24, topPadding: 0)
         #elseif os(watchOS)
-        return MeasuringCompleteConfig(dotSize: 10, fontSize: 12, topPadding: 20)
+        return MeasuringCompleteConfig(dotSize: 10, fontSize: 14, topPadding: 20)
         #endif
     }
 }
